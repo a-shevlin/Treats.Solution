7797148cfaa8470cc86f5c3b40bae2d979032df0
Alex Shevlin <alexshevlin1@gmail.com>
2022-08-12 10:08:38 -0700

    create HomeController.cs

---
+++ Treats/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Treats.Models;
+using System.Dynamic;
+using System.Linq;
+
+namespace Treats.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly TreatsContext _db;
+
+    public HomeController(TreatsContext db)
+    {
+      _db = db;
+    }
+    [HttpGet("/")]
+    public ActionResult Index() 
+    { 
+      dynamic model = new ExpandoObject();
+      model.Treat = _db.Treats.ToList();
+      model.Flavor = _db.Flavors.ToList();
+      return View(model); 
+    }
+  }
+}
